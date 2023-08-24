6e26876386fc894dec98678c59d94a502dfda35f
Anton Kabanovs <akabanovs@anaconda.com>
2023-08-25 00:23:58 +0100

    WIP, i.e. no ebm support, no pytests

---
+++ recipe/build_interpret-core.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+$PYTHON -m pip install ./python/interpret-core/ -vv --no-deps --no-build-isolation
+
---
+++ recipe/build_interpret.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+$PYTHON -m pip install ./python/interpret -vv --no-deps --no-build-isolation
