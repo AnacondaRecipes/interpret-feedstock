51de90bba81b3cdca08782b9f79522565672a116
Anton Kabanovs <akabanovs@anaconda.com>
2023-08-25 14:52:56 +0100

    fix osx

--- recipe/build-libebm.sh
+++ recipe/build-libebm.sh
@@ -3,7 +3,7 @@
 /bin/sh ./build.sh 
 
 if [[ $(uname) == Darwin ]]; then
-   cp ./python/interpret-core/interpret/lib/libebm_mac_arm.dylib "$PREFIX/lib/libebm.dylib"
+   cp ./python/interpret-core/interpret/lib/libebm_mac_x64.dylib "$PREFIX/lib/libebm.dylib"
 else
    cp ./python/interpret-core/interpret/lib/libebm_linux_x64.so "$PREFIX/lib/libebm.so"
 fi
